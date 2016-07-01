9a27d70a2a70c99eff9c44c17864140435644559
shoumiao_yao@kingdee.com <mingwen_luo@kingdee.com>
2016-07-01 15:10:22 +0800

    基金爬虫数据

---
+++ src/main/java/com/jxnu/fundCrawler/business/store/FundStore.java
@@ -0,0 +1,27 @@
+package com.jxnu.fundCrawler.business.store;
+
+import com.jxnu.fundCrawler.business.model.Fund;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author shoumiao_yao
+ * @date 2016-07-01
+ */
+@Component
+public class FundStore extends BaseStore {
+    private Logger logger = LoggerFactory.getLogger(FundStore.class);
+
+    /**
+     * 插入基金
+     *
+     * @param fundList
+     */
+    public void insertFund(List<Fund> fundList) {
+        int length = template.insert("fund.insertFund", fundList);
+        logger.info("insert fund {}", length);
+    }
+}
