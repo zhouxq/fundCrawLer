6772a8ced4ba86adc5d8b0d99ddf1ef71ba6d69f
ysm20140508 <1247793952@qq.com>
2016-07-02 10:43:20 +0800

    解决基金经理人bug

--- src/main/java/com/jxnu/fundCrawler/thread/grab/FundThread.java
+++ src/main/java/com/jxnu/fundCrawler/thread/grab/FundThread.java
@@ -5,6 +5,9 @@ import com.jxnu.fundCrawler.business.model.Fund;
 import com.jxnu.fundCrawler.business.store.CompanyStore;
 import com.jxnu.fundCrawler.business.store.FundStore;
 import com.jxnu.fundCrawler.utils.OkHttpUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.jsoup.helper.StringUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -34,36 +37,42 @@ public class FundThread implements Runnable {
     public void run() {
         List<Company> companyList = companyStore.queryAll();
         for (Company company : companyList) {
-            String companyCode=company.getCode().toString();
-            String url2 = url.replace("#", companyCode);
-            Document document = OkHttpUtils.parseToDocument(url2, "gb2312");
-            if (document==null) continue;
-            Elements tbodys = document.select("tbody");
-            Element element = tbodys.get(6);
-            if(element.select("td").first().select("a") !=null){
-                element=tbodys.get(7);
-            }
-            Elements trs = element.select("tr");
-            List<Fund> fundList = new ArrayList<Fund>();
-            for (int index = 2; index < trs.size(); index++) {
-                Fund fund = new Fund();
-                fund.setCompanyCode(companyCode);
-                fund.setCompanyName(company.getName());
-                Element tr = trs.get(index);
-                Elements tds = tr.select("td");
-                String[] values = tds.get(0).text().split(" ");
-                String fundName = values[0];
-                fund.setName(fundName);
-                String fundCode = values[1];
-                fund.setCode(fundCode);
-                String type = tds.get(2).text();
-                fund.setType(type);
-                String handler = tds.get(9).text();
-                fund.setHandler(handler);
-                fundList.add(fund);
-            }
-            if (!fundList.isEmpty()) {
-                fundStore.insertFund(fundList);
+            try {
+                String companyCode = company.getCode().toString();
+                String url2 = url.replace("#", companyCode);
+                Document document = OkHttpUtils.parseToDocument(url2, "gb2312");
+                if (document == null) continue;
+                Elements tbodys = document.select("tbody");
+                Element element = tbodys.get(6);
+                Elements a = element.select("td").first().select("a");
+                if (a != null && a.size() > 0) {
+                    element = tbodys.get(7);
+                }
+                Elements trs = element.select("tr");
+                List<Fund> fundList = new ArrayList<Fund>();
+                for (int index = 2; index < trs.size(); index++) {
+                    Fund fund = new Fund();
+                    fund.setCompanyCode(companyCode);
+                    fund.setCompanyName(company.getName());
+                    Element tr = trs.get(index);
+                    Elements tds = tr.select("td");
+                    String[] values = tds.get(0).text().split(" ");
+                    String fundName = values[0];
+                    fund.setName(fundName);
+                    String fundCode = values[1];
+                    fund.setCode(fundCode);
+                    String type = tds.get(2).text();
+                    fund.setType(type);
+                    String handler = tds.get(9).text();
+                    handler = StringUtils.remove(handler, "等");
+                    fund.setHandler(handler.trim());
+                    fundList.add(fund);
+                }
+                if (!fundList.isEmpty()) {
+                    fundStore.insertFund(fundList);
+                }
+            } catch (Exception e) {
+                logger.error("error:{}", ExceptionUtils.getMessage(e));
             }
         }
     }
