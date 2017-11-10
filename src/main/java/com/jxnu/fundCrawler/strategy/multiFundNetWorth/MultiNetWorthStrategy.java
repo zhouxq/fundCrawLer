9e62f3124c310841b5b252f95a8f91f876223d5d
yaphyao <Fengqile@2017>
2017-11-10 18:13:29 +0800

    优化策略

--- src/main/java/com/jxnu/fundCrawler/strategy/multiFundNetWorth/MultiNetWorthStrategy.java
+++ src/main/java/com/jxnu/fundCrawler/strategy/multiFundNetWorth/MultiNetWorthStrategy.java
@@ -1,9 +1,18 @@
 package com.jxnu.fundCrawler.strategy.multiFundNetWorth;
 
+import com.jxnu.fundCrawler.business.model.FundNetWorth;
+
+import java.util.List;
+
 /**
  * @author yaphyao
  * @version 2017/11/10
  * @see com.jxnu.fundCrawler.strategy.multiFundNetWorth
  */
 public abstract class MultiNetWorthStrategy {
+    private MultiNetWorthStrategy next;
+
+    public void handler(List<FundNetWorth> fundNetWorthList) {
+
+    }
 }
