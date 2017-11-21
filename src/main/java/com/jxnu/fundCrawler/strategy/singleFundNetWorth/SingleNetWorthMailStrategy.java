1e0b3cfa9ba22a840c851c9df3c8f38502594a0e
yaoshoumiao <0605KLI>
2017-11-21 21:42:42 +0800

    增加基金邮件

--- src/main/java/com/jxnu/fundCrawler/strategy/singleFundNetWorth/SingleNetWorthMailStrategy.java
+++ src/main/java/com/jxnu/fundCrawler/strategy/singleFundNetWorth/SingleNetWorthMailStrategy.java
@@ -1,19 +1,20 @@
 package com.jxnu.fundCrawler.strategy.singleFundNetWorth;
 
 import com.google.common.collect.Lists;
+import com.jxnu.fundCrawler.business.model.Fund;
 import com.jxnu.fundCrawler.business.model.FundNetWorth;
 import com.jxnu.fundCrawler.business.model.Mail;
 import com.jxnu.fundCrawler.business.model.MailFundStatus;
 import com.jxnu.fundCrawler.business.store.FundNetWorthStore;
+import com.jxnu.fundCrawler.business.store.FundStore;
+import com.jxnu.fundCrawler.utils.MailUtil;
 import com.jxnu.fundCrawler.utils.NumberUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author yaphyao
@@ -24,6 +25,8 @@ import java.util.List;
 public class SingleNetWorthMailStrategy extends BaseSingleNetWorthStrategy {
     @Autowired
     private FundNetWorthStore fundNetWorthStore;
+    @Autowired
+    private FundStore fundStore;
 
     @PostConstruct
     public void init() {
@@ -39,6 +42,8 @@ public class SingleNetWorthMailStrategy extends BaseSingleNetWorthStrategy {
         //两个月最小净值
         Float minNetWorth = fundNetWorthStore.queryPeriodMin(code);
         List<Mail> mailList = new ArrayList<Mail>();
+        Set<Fund> upFunds = new HashSet<Fund>();
+        Set<Fund> downFunds = new HashSet<Fund>();
         for (FundNetWorth fundNetWorth : fundNetWorthList) {
             Float netWorth = fundNetWorth.getNetWorth();
             if (NumberUtil.maxRatio(netWorth, maxNetWorth)) {
@@ -49,6 +54,7 @@ public class SingleNetWorthMailStrategy extends BaseSingleNetWorthStrategy {
                     mail.setCode(code);
                     mail.setType(MailFundStatus.DOWN.getIndex());
                     mailList.add(mail);
+                    downFunds.add(fundStore.findById(code));
                 }
             }
             if (NumberUtil.minRatio(netWorth, minNetWorth)) {
@@ -59,14 +65,22 @@ public class SingleNetWorthMailStrategy extends BaseSingleNetWorthStrategy {
                     mail.setType(MailFundStatus.UP.getIndex());
                     mail.setCode(code);
                     mailList.add(mail);
+                    upFunds.add(fundStore.findById(code));
                 }
             }
         }
         if (!mailList.isEmpty()) {
             fundNetWorthStore.insertMail(mailList);
         }
+        if (!downFunds.isEmpty()) {
+            MailUtil.sendmail("基金下降", downFunds);
+        }
+        if (!upFunds.isEmpty()) {
+            MailUtil.sendmail("基金上升", upFunds);
+        }
         if (super.next != null) {
             super.next.handler(fundNetWorthList);
         }
     }
+
 }
