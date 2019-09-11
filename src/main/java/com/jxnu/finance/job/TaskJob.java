755c598740ee40989eb5b7e37f99438d869321b4
zhouxingqi <402447928@qq.com>
2019-09-11 18:09:48 +0800

    SQL

--- src/main/java/com/jxnu/finance/job/TaskJob.java
+++ src/main/java/com/jxnu/finance/job/TaskJob.java
@@ -1,16 +1,18 @@
 package com.jxnu.finance.job;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Configurable
 @Component
 @EnableScheduling
 public class TaskJob {
 
 //    @Scheduled(cron = "*/5 * * * * *")
-    @Scheduled(fixedRate=5000)
+    @Scheduled(fixedRate=1000)
     public void job() {
         System.out.println("JOB……");
     }
